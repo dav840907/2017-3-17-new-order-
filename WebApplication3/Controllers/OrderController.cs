ef6d5e9d81a1c0a057af3cc493809b670f351339
dav840907 <dav840907@gmail.com>
2017-03-24 15:02:17 +0800

    tempData

--- WebApplication3/Controllers/OrderController.cs
+++ WebApplication3/Controllers/OrderController.cs
@@ -11,10 +11,12 @@ namespace WebApplication3.Controllers
         // GET: Order
         public ActionResult Index()
         {
-            Models.OrderService orderService = new Models.OrderService();
-            var order = orderService.GerOrderById("111");
-            ViewBag.CustId = order.CustId;
-            ViewBag.CustName = order.CustName;
+            //Models.OrderService orderService = new Models.OrderService();
+            //  var order = orderService.GerOrderById("111");
+            //  ViewBag.CustId = order.CustId;
+            //  ViewBag.CustName = order.CustName;
+            // return View();
+         
             return View();
         }
         public ActionResult Index2(string id)
@@ -22,10 +24,26 @@ namespace WebApplication3.Controllers
             ViewBag.id = id;
             return View();
         }
+
+
         public ActionResult InsertOrder()
         {
 
             return View();
         }
+
+        /// <summary>
+        /// 新增訂單存檔的Action
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order) {
+            ViewBag.Desc1 = "ViewBag";
+            ViewData["Desc2"] = "ViewData";
+            TempData["Desc3"] = "TempData";
+            return RedirectToAction("Index");
+        }
+
     }
 }
